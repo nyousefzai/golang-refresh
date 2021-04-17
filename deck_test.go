d98c7af0b9452681e07ce4d9433dd8014ca612ff
Navid <navid22y@gmail.com>
2021-04-17 08:47:06 -0700

    adding read from file test

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,12 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T){
 	d := newDeck()
-
 
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
@@ -16,5 +18,18 @@ func TestNewDeck(t *testing.T){
 
 	if d[len(d) - 1] != "Four of Clubs"{
 		t.Errorf("Expected last card of Four of Clubs, but got %v",d[len(d) - 1 ])
+	}
+}
+
+
+func TestDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in dec, got %v",len(loadedDeck))
 	}
 }
